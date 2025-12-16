8b03079710e78769241dd8e4e7a78ecf42f23d85
Michael Hy <antiha8erz@gmail.com>
2025-12-16 14:42:06 -0800

    Fantastic 4 first commit

---
+++ src/main/java/com/revature/fantastic4/Fantastic4Application.java
@@ -0,0 +1,13 @@
+package com.revature.fantastic4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fantastic4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fantastic4Application.class, args);
+	}
+
+}
