a1c29b23dae670c4a063df12bb21ea93822b6893
Michael Hy <antiha8erz@gmail.com>
2025-12-16 14:42:06 -0800

    Fantastic 4 first commit

---
+++ src/test/java/com/revature/fantastic4/Fantastic4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.fantastic4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fantastic4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
