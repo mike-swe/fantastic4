893aeea5f8a362ff54b9067d0ca187f6445aeccd
Xing <xc1222@gmail.com>
2025-12-19 15:21:32 -0500

    application.properties and build.gradle to match in class

---
+++ src/main/java/com/revature/fantastic4/Admin.java
@@ -0,0 +1,14 @@
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = 'admins')
+Public class Admin {
+    @Id
+    @Column(name = 'admin_id', nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID adminID;
+    @Column
+    private String username;
+    @Column
+    private String password;
+}
---
+++ src/main/java/com/revature/fantastic4/Tester.java
@@ -0,0 +1,14 @@
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = 'tester')
+public class Tester {
+    @Id
+    @Column(name = 'tester_id', nullable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID testerID;
+    @Column
+    private String testerUsername;
+    @Column
+    private String testerPswd;
+}
