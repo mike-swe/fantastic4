a7b7e20668f8b710309a5e8caabf967b1dceb04b
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2025-12-23 15:32:25 -0800

    Added import statements to issue repo and project repo

--- src/main/java/com/revature/fantastic4/repository/ProjectRepository.java
+++ src/main/java/com/revature/fantastic4/repository/ProjectRepository.java
@@ -1,4 +1,6 @@
-
+package com.revature.fantastic4.repository;
+import com.revature.fantastic4.entity.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, UUID>
 {
