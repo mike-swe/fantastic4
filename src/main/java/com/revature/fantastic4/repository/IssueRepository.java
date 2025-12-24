c647900ef7f465214c7e95fe51a8ccf137f2dd7e
Xing <xc1222@gmail.com>
2025-12-23 22:56:17 -0500

    user repo, imports, fixing errors in issue repo + project repo

--- src/main/java/com/revature/fantastic4/repository/IssueRepository.java
+++ src/main/java/com/revature/fantastic4/repository/IssueRepository.java
@@ -1,11 +1,20 @@
 package com.revature.fantastic4.repository;
+
+
 import com.revature.fantastic4.entity.Issue;
+import com.revature.fantastic4.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
 
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, UUID>
 {
-    List<Issue> findByCreator(User creator);
+    List<Issue> findByCreatedBy(User createdBy);
 
-    List<Issue> findByAssignee(User assignee);
+    //List<Issue> findByAssignedTo(User assignedTo);
+    //We dont actually have an assignedto field in Issue
 }
