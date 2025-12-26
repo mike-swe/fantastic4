e4f444824f161a424635a1303923b876f15ad8ce
Michael Hy <antiha8erz@gmail.com>
2025-12-26 12:18:44 -0800

    comment out code

---
+++ src/main/java/com/revature/fantastic4/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.revature.fantastic4.repository;
+
+
+import com.revature.fantastic4.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
