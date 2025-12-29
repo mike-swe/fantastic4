f262b1644558dca5c3615a31ef8873b0855844b6
Michael Hy <antiha8erz@gmail.com>
2025-12-29 08:54:15 -0800

    setup controllers and jwt for login

--- backend/src/main/java/com/revature/fantastic4/repository/UserRepository.java
+++ backend/src/main/java/com/revature/fantastic4/repository/UserRepository.java
@@ -5,17 +5,15 @@ import com.revature.fantastic4.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.revature.fantastic4.enums.Role;
-import com.revature.fantastic4.entity.ProjectAssignment;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     List<User> findByRole(Role role);
-    boolean existsByUsernameAndRole(String uname, Role role);
-    Set<ProjectAssignment> getAllProjectsAssignedToUser(User user);
-
+    boolean existsByUsernameAndRole(String username, Role role);
+    Optional<User> findByUsername(String username);
 
 }
