0561d1a06c237c85082d0c3251b559e472407f25
nitinpinnamaneni <n_pinnamaneni@u.pacific.edu>
2025-12-30 12:54:57 -0800

    Implemented repository methods from Project and AuditLog

--- backend/src/main/java/com/revature/fantastic4/service/AuditService.java
+++ backend/src/main/java/com/revature/fantastic4/service/AuditService.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //package com.revature.fantastic4.service;
 //
 //import com.revature.fantastic4.entity.AuditLog;
@@ -26,3 +27,48 @@
 //        return auditLogRepository.save(log);
 //    }
 //}
+=======
+package com.revature.fantastic4.service;
+
+import com.revature.fantastic4.entity.AuditLog;
+import com.revature.fantastic4.repository.AuditLogRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuditService 
+{
+
+    private final AuditLogRepository auditLogRepository;
+
+    public AuditService(AuditLogRepository auditLogRepository) 
+    {
+        this.auditLogRepository = auditLogRepository;
+    }
+
+    public AuditLog log(Long actorId, String action, String entityType, Long entityId, String details) 
+    {
+        AuditLog log = new AuditLog();
+        log.setActorUserId(actorId);
+        log.setAction(action);
+        log.setEntityType(entityType);
+        log.setEntityId(entityId);
+        log.setDetails(details);
+        return auditLogRepository.save(log);
+    }
+
+    public List<AuditLog> getAllLogsByActor(Long actorId) {
+        List<AuditLog> logsByActor = auditLogRepository.findByActorUserId(actorId);
+        return logsByActor;
+    }
+
+    public List<AuditLog> getAllLogsByAction(String action) {
+        List<AuditLog> logsByAction = auditLogRepository.findByAction(action);
+        return logsByAction;
+    }
+
+    public List<AuditLog> getAllLogsByEntityType(String entityType) {
+        List<AuditLog> logsByEntityType = auditLogRepository.findByEntityType(entityType);
+        return logsByEntityType;
+    }
+}
+>>>>>>> c55563e (Implemented repository methods from Project and AuditLog)
