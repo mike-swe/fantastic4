ece39aac7f40500e0e54fec5710c7c7da22be2c1
Xing <xc1222@gmail.com>
2026-01-13 12:16:56 -0500

    login() test created

--- frontend/src/app/services/auth-service.spec.ts
+++ frontend/src/app/services/auth-service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { AuthService } from './auth-service';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        AuthService,
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
+
     service = TestBed.inject(AuthService);
+    httpMock = TestBed.inject(HttpTestingController)
+
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should retuyrn a token on successful login', () => {
+    const mockResponse = {token: 'matikanetannhauser'};
+
+    service.login('test' , 'password123').subscribe(res => {
+      expect(res.token).toBe('matikanetannhauser');
+    })
+
+    const req = httpMock.expectOne('http://localhost:8080/users/login')
+    req.flush(mockResponse);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 });
