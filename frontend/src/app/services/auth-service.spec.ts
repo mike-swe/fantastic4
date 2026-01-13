2161c653b64771d2d1e6e4bfa1cc954a863e6bba
Xing <xc1222@gmail.com>
2026-01-13 13:23:09 -0500

    unit test logout(), getToken(), failtest

--- frontend/src/app/services/auth-service.spec.ts
+++ frontend/src/app/services/auth-service.spec.ts
@@ -2,17 +2,24 @@ import { TestBed } from '@angular/core/testing';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { AuthService } from './auth-service';
 import { provideHttpClient } from '@angular/common/http';
+import { JwtStorage } from './jwt-storage.service';
 
 describe('AuthService', () => {
   let service: AuthService;
   let httpMock: HttpTestingController;
+  const mockJwtStorage = {
+    setToken: vi.fn(),
+    getToken: vi.fn(),
+    clearToken: vi.fn()
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         AuthService,
         provideHttpClient(),
-        provideHttpClientTesting()
+        provideHttpClientTesting(),
+        {provide: JwtStorage, useValue: mockJwtStorage}
       ]
     });
 
@@ -21,18 +28,43 @@ describe('AuthService', () => {
 
   });
 
-  it('should retuyrn a token on successful login', () => {
-    const mockResponse = {token: 'matikanetannhauser'};
+  afterEach(() => {
+    vi.clearAllMocks();
+    httpMock.verify();
+  });
 
-    service.login('test' , 'password123').subscribe(res => {
+  //UNIT TEST : login()
+  it('should return a token on successful login', () => {
+    const mockResponse = {token: 'matikanetannhauser'};
+    
+
+    service.login('mambo' , 'password123').subscribe(res => {
       expect(res.token).toBe('matikanetannhauser');
     })
 
     const req = httpMock.expectOne('http://localhost:8080/users/login')
     req.flush(mockResponse);
+
+    expect(mockJwtStorage.setToken).toHaveBeenCalledWith("matikanetannhauser")
   });
 
-  afterEach(() => {
-    httpMock.verify();
+  //UNIT TEST : logout()
+  it('should clear the token from storage when logout is called', () => {
+    service.logout();
+    expect(mockJwtStorage.clearToken).toHaveBeenCalled();
   });
+
+  //UNIT TEST : getToken()
+  it('should get the token from storage when called', () => {
+    service.getToken();
+    expect(mockJwtStorage.getToken).toHaveBeenCalled();
+  });
+
+
+  //THIS TEST SHOULD FAIL
+  it('this test should fail because 1 is not 2', () => {
+  expect(1).toBe(2);
+  });
+
+
 });
